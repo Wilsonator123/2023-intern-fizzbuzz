858916a3a001e54f0df8ee7ae9a9fd849247c160
GeoWil <George.Wilson@softwire.com>
2023-06-26 17:24:11 +0100

    Part 1 done

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,7 +3,16 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     // Put your code here...
-
+    for(let i =1; i<100; i++){
+        let str = "";
+        if(i%3===0){
+            str+="Fizz"
+        }
+        if(i%5===0){
+            str+="Buzz"
+        }
+        console.log(str === "" ? i : str)
+    }
 }
 
 // Now, we run the main function:
