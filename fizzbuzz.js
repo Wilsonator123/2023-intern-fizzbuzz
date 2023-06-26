e1014e80c5044f1a477b09bb75fd9dd445e249d0
GeoWil <George.Wilson@softwire.com>
2023-06-26 17:32:18 +0100

    Started part 2 :)

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,16 +3,34 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     // Put your code here...
+    // for(let i =1; i<100; i++){
+    //     let str = "";
+    //     if(i%3===0) str+="Fizz"
+    //
+    //     if(i%13===0) str+="Fezz"
+    //
+    //     if(i%5===0) str+="Buzz"
+    //
+    //     if(i%11===0)
+    //         str=""
+    //         if(i%13) str+="Fezz";
+    //         str+="Bong"
+    //     console.log(str === "" ? i : str)
+    // }
     for(let i =1; i<100; i++){
-        let str = "";
-        if(i%3===0){
-            str+="Fizz"
+            let arr = "";
+            if(i%3===0) arr.push("Fizz");
+
+            if(i%13===0) str+="Fezz"
+
+            if(i%5===0) str+="Buzz"
+
+            if(i%11===0)
+                str=""
+                if(i%13) str+="Fezz";
+                str+="Bong"
+            console.log(str === "" ? i : str)
         }
-        if(i%5===0){
-            str+="Buzz"
-        }
-        console.log(str === "" ? i : str)
-    }
 }
 
 // Now, we run the main function:
